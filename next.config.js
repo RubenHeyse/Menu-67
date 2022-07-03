10f9a617fb2220cac13bb0adc2eba031fb90271a
RubenHeyse <50083619+RubenHeyse@users.noreply.github.com>
2022-07-03 10:56:17 +0100

    Updated image endpoint

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ module.exports = {
 
   images: {
     domains: [
-      'media.graphcms.com', 
+      'media.graphassets.com', 
       'images.vexels.com'
     ],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
